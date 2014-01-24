6a199bc5c97c87f9952f5862cd30f99e2ee079b3
esbb48 <4j1k3>
2014-01-24 23:01:00 +0800

    Add prototype

--- vendor/js/map.js
+++ vendor/js/map.js
@@ -2,117 +2,98 @@
           google:false;*/
 (function () {
 "use strict";
-  var geocoder,
-      googleMap;
-
-      geocoder = new google.maps.Geocoder();
+  var googleMap;
       googleMap = new GoogleMap();
 
   function GoogleMap() {
-    var _map,
-        _markers = [];
+    this._geocoder = new google.maps.Geocoder();
+    this._map = null;
+    this._markers = [];
+  }
 
-    this.addMarker = function(location, title) {
-      var marker,
-          markerParameter = [];
+  GoogleMap.prototype.addMarker = function(location, title) {
+    var marker,
+        markerParameter = [];
 
-      markerParameter = {
-        map: _map,
-        position: location,
-        title: title
+    markerParameter = {
+      map: googleMap._map,
+      position: location,
+      title: title
+    };
+
+    marker = new google.maps.Marker(markerParameter);
+    googleMap._markers.push(marker);
+  };
+
+  GoogleMap.prototype.goLoop = function(textArr, i){
+    i--;
+    if (i < 0) {
+
+    } else if (textArr[i].length <= 0){
+      googleMap.goLoop(textArr, i);
+    } else {
+      googleMap._geocoder.geocode(
+        {"address":textArr[i]},
+        googleMap.onGeocodeGet
+      );
+
+      setTimeout(function () {
+        googleMap.goLoop(textArr, i);
+      }, 1000);
+    }
+  };
+
+  GoogleMap.prototype.initialize = function() {
+    var mapDiv,
+      mapOptions = [],
+      TAIWAN_LAT = 23.654587852202987,
+      TAIWAN_LNG = 121.014404296875,
+      zoom = 8;
+
+      mapOptions = {
+        zoom: zoom,
+        center: new google.maps.LatLng(TAIWAN_LAT, TAIWAN_LNG)
       };
 
-      marker = new google.maps.Marker(markerParameter);
-      _markers.push(marker);
-    };
+      mapDiv = $("#map-canvas")[0];
+      googleMap._map = new google.maps.Map(mapDiv, mapOptions);
 
-    this.getMap = function () {
-      return _map;
-    };
+      $("#address").on("change",
+        function () {
+          var i,
+              str,
+              textArr;
 
-    this.getMarkers = function () {
-      return _markers;
-    };
-
-    this.goLoop = function(textArr, i){
-      i--;
-      if (i < 0) {
-
-      } else if (textArr[i].length <= 0){
-        googleMap.goLoop(textArr, i);
-      } else {
-        geocoder.geocode(
-          {"address":textArr[i]},
-          googleMap.onGeocodeGet
-        );
-
-        setTimeout(function () {
+          str = $("#address").val();
+          textArr = str.split("\n");
+          i = textArr.length;
           googleMap.goLoop(textArr, i);
-        }, 1000);
-      }
-    };
+        }
+      );
+  };
 
-    this.initialize = function() {
-      var mapDiv,
-        mapOptions = [],
-        TAIWAN_LAT = 23.654587852202987,
-        TAIWAN_LNG = 121.014404296875,
-        zoom = 8;
+  GoogleMap.prototype.onGeocodeGet = function(results, status) {
+    var address,
+        LatLng;
+    if (status == google.maps.GeocoderStatus.OK) {
+      address = results[0].formatted_address;
+      LatLng = results[0].geometry.location;
+      googleMap._map.setCenter(LatLng);  //將地圖中心定位到查詢結果
+      googleMap.addMarker(LatLng, address);
+    }
+  };
 
-        mapOptions = {
-          zoom: zoom,
-          center: new google.maps.LatLng(TAIWAN_LAT, TAIWAN_LNG)
-        };
-
-        mapDiv = $("#map-canvas")[0];
-        _map = new google.maps.Map(mapDiv, mapOptions);
-
-        $("#address").on("change",
-          function () {
-            var i,
-                str,
-                textArr;
-
-            str = $("#address").val();
-            textArr = str.split("\n");
-            i = textArr.length;
-            googleMap.goLoop(textArr, i);
-          }
-        );
-    };
-
-    this.onGeocodeGet = function(results, status) {
-      var address,
-          LatLng;
-      if (status == google.maps.GeocoderStatus.OK) {
-        address = results[0].formatted_address;
-        LatLng = results[0].geometry.location;
-        _map.setCenter(LatLng);  //將地圖中心定位到查詢結果
-        googleMap.addMarker(LatLng, address);
-      }
-    };
-
-    this.setAllMap = function(factor) {
-      var i = 0;
-      for (i; i < _markers.length; i++) {
-        _markers[i].setMap(factor);
-      }
-    };
-
-    this.setMapValue = function (factor) {
-      _map = factor;
-    };
-
-    this.setMarkers = function (factor) {
-      _markers = factor;
-    };
-
-  }
+  GoogleMap.prototype.setAllMap = function(factor) {
+    var i = 0;
+    for (i; i < googleMap._markers.length; i++) {
+      googleMap._markers[i].setMap(factor);
+    }
+  };
 
   $("#delete").on("click",
     function () {
-      googleMap.setAllMap(null)
-              .setMarkers([]);
+      googleMap.setAllMap(null);
+      googleMap._markers = [];
     }
   );
 
@@ -125,7 +106,7 @@
   $("#show").on("click",
     function () {
       var map;
-      map = googleMap.getMap();
+      map = googleMap._map;
       googleMap.setAllMap(map);
     }
   );
