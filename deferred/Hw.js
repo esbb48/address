bcf8ab8b7f208bfa4ace3a0013cf06fa3052a493
esbb48 <4j1k3>
2014-03-12 23:52:06 +0800

    Update deferred hw

--- deferred/Hw.js
+++ deferred/Hw.js
@@ -19,25 +19,51 @@
     var base = "/address/deferred/data.php";
     var deferred;
     var j = 1;
+    var thenResult;
 
-    $.getJSON(base)
-        .done(function(data) {
+    deferred = $.getJSON(base)
+        .then(function(data) {
             console.log(data);
-            goLoop(data);
+             return $.getJSON(base + "?id=" + data.number);
         });
 
-    function goLoop(data) {
-        if ( j < 10) {
-            j++;
-            $.getJSON(base + "?id=" + data.number)
-            .done(function(data){
-                console.log(data);
-                goLoop(data);
-            })
-            .fail(function(data){
-                alert('fail');
-            });
-        }
+    function doneFilter(){
+        alert('done');
     }
+
+    function failFilter(){
+        alert('fail');
+    }
+
+    function progressFilter(){
+        alert('progress');
+    }
+
+    function then(deferred, doneFilter, failFilter, progressFilter) {
+        var tempDeferred;
+        tempDeferred = new jQuery.Deferred();
+        tempDeferred.done = doneFilter;
+        tempDeferred.fail = failFilter;
+        tempDeferred.progress = progressFilter;
+        tempDeferred.then = deferred;
+        return tempDeferred;
+    }
+
+    thenResult = then(deferred, doneFilter, failFilter, progressFilter);
+    debugger;
+
+    // function goLoop(data) {
+    //     if ( j < 10) {
+    //         j++;
+    //         $.getJSON(base + "?id=" + data.number)
+    //         .done(function(data){
+    //             console.log(data);
+    //             goLoop(data);
+    //         })
+    //         .fail(function(data){
+    //             alert('fail');
+    //         });
+    //     }
+    // }
 
 })(this);
