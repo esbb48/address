65bba29140df4010de7ea6ce02fa86ada40ad45e
esbb48 <4j1k3>
2014-03-15 14:01:47 +0800

    Update Deferred

--- deferred/Hw.js
+++ deferred/Hw.js
@@ -1,69 +1,59 @@
 (function(global, undefined) {
     "use strict";
 
-    var locations,
-        users;
+    // var locations,
+    //     users;
 
-    users = [
-        1,
-        2,
-        3
-    ];
+    // users = [
+    //     1,
+    //     2,
+    //     3
+    // ];
 
-    locations = users.map(function(user) {
-        return "/address/deferred/data.php?id=" + user;
-    });
+    // locations = users.map(function(user) {
+    //     return "/address/deferred/data.php?id=" + user;
+    // });
 
-    console.log(locations);
+    // // console.log(locations);
 
-    var base = "/address/deferred/data.php";
-    var deferred;
-    var j = 1;
-    var thenResult;
+    // var base = "/address/deferred/data.php";
+    // var deferred;
+    // var j = 1;
+    // var thenResult;
 
-    deferred = $.getJSON(base)
-        .then(function(data) {
-            console.log(data);
-             return $.getJSON(base + "?id=" + data.number);
-        });
+    // deferred = $.getJSON(base)
+    //     .then(function(data) {
+    //         console.log(data);
+    //          return $.getJSON(base + "?id=" + data.number);
+    //     });
 
     function doneFilter(){
-        alert('done');
+        alert("done");
     }
 
     function failFilter(){
-        alert('fail');
+        alert("fail");
     }
 
     function progressFilter(){
-        alert('progress');
+        alert("progress");
     }
 
     function then(deferred, doneFilter, failFilter, progressFilter) {
         var tempDeferred;
-        tempDeferred = new jQuery.Deferred();
-        tempDeferred.done = doneFilter;
-        tempDeferred.fail = failFilter;
-        tempDeferred.progress = progressFilter;
-        tempDeferred.then = deferred;
+        tempDeferred = deferred.done(doneFilter)
+                        .fail(failFilter)
+                        .progress(progressFilter);
+
         return tempDeferred;
     }
 
-    thenResult = then(deferred, doneFilter, failFilter, progressFilter);
-    debugger;
+      var head;
 
-    // function goLoop(data) {
-    //     if ( j < 10) {
-    //         j++;
-    //         $.getJSON(base + "?id=" + data.number)
-    //         .done(function(data){
-    //             console.log(data);
-    //             goLoop(data);
-    //         })
-    //         .fail(function(data){
-    //             alert('fail');
-    //         });
-    //     }
-    // }
+      // head = jQuery.Deferred().resolve();
+      head = jQuery.Deferred().reject();
+      head = then(head, doneFilter, failFilter, progressFilter);
+      head = then(head, doneFilter, failFilter, progressFilter);
+      head = then(head, doneFilter, failFilter, progressFilter);
 
 })(this);
